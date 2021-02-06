183425b1818340fd119c20c9004eaeba8c518e0a
m-a-hossain <adam.eve007@gmail.com>
2021-02-07 03:00:06 +0600

    finding food name , id, img

---
+++ script.js
@@ -0,0 +1,6 @@
+/* const searchFood= document.getElementById('inputItem')
+console.log(searchFood)
+document.getElementById('search').addEventListener('click', findFood())
+   function findFood(){
+       alert('ki')
+   } */
