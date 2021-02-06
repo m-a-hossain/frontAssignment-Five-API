8b87af06094c39e8844d689395ff7bbdeb7d686b
m-a-hossain <adam.eve007@gmail.com>
2021-02-07 03:02:15 +0600

    commit for js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-/* const searchFood= document.getElementById('inputItem')
-console.log(searchFood)
-document.getElementById('search').addEventListener('click', findFood())
-   function findFood(){
-       alert('ki')
-   } */
+const searchFood= document.getElementById('inputItem')
+document.getElementById('search').addEventListener('click', findFood)
+function findFood(){
+ let foodName= searchFood.value.trim()
+ //console.log(searchFood.value.trim())
+
+ fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${foodName}`)
+.then(res => res.json())
+.then(data=> {
+  for(let i=0; i<data.meals.length;i++){
+   let foodCount=data.meals[i]
+   console.log(foodCount.idMeal,foodCount.strMeal,foodCount.strMealThumb)
+  }
+  /* console.log(data.meals)
+  const allMeal =data.meals
+  const idOfMeal=allMeal.idMeal
+  console.log(idOfMeal)
+  let mealName= allMeal.strMeal
+  console.log(mealName)
+  let img = allMeal.strMealThumb
+  console.log(img) */
+//  const getName= meals[0]
+ // console.log(getName)
+})
+} 
